9edb7963697326a59ad2ccfd5f2ed2b6e27a10a7
bushrakalaji <91549377+bushrakalaji@users.noreply.github.com>
2022-10-18 23:47:21 +0200

    Update update.mjs

--- src/js/api/posts/update.mjs
+++ src/js/api/posts/update.mjs
@@ -10,16 +10,15 @@ const method = "put";
  * @returns
  */
 export async function updatePost(postData) {
-  
-    if (!postData.id) {
-      throw new Error("Update requires a postID");
-    }
-    const updatePostURL = `${API_SOCIAL_URL}${action}/${postData.id} `;
-    const response = await authFetch(updatePostURL, {
-      method,
-      body: JSON.stringify(postData),
-    });
-    alert("Your post has updated");
-    window.location.replace("/posts/index.html");
-    return await response.json();
+  if (!postData.id) {
+    throw new Error("Update requires a postID");
+  }
+  const updatePostURL = `${API_SOCIAL_URL}${action}/${postData.id} `;
+  const response = await authFetch(updatePostURL, {
+    method,
+    body: JSON.stringify(postData),
+  });
+  alert("Your post has updated");
+  window.location.replace("/posts/index.html");
+  return await response.json();
 }
